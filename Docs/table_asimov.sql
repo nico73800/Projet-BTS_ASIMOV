f6dd984012df9a43595843cb3ae77b165ce87efe
nico73800 <50556174+nico73800@users.noreply.github.com>
2023-03-21 10:27:41 +0100

    Add files via upload

---
+++ Docs/table_asimov.sql
@@ -0,0 +1,64 @@
+DROP DATABASE IF EXISTS Asimov;
+Create database Asimov;
+use Asimov;
+
+CREATE TABLE Professeur(
+   idProfesseur INT,
+   nomProfesseur VARCHAR(50) NOT NULL,
+   prenomProfesseur VARCHAR(50) NOT NULL,
+   mailProfesseur VARCHAR(50) NOT NULL,
+   estReferent boolean default false,
+   estProviseur boolean NOT NULL Default false,
+   PRIMARY KEY(idProfesseur),
+   UNIQUE(mailProfesseur)
+);
+
+CREATE TABLE Matiere(
+   idMatiere INT,
+   libelle VARCHAR(50),
+   PRIMARY KEY(idMatiere)
+);
+
+CREATE TABLE Section(
+   idSection INT,
+   libelleSection VARCHAR(50) NOT NULL,
+   anneeSection VARCHAR(50),
+   idProfesseur INT NOT NULL,
+   PRIMARY KEY(idSection),
+   UNIQUE(libelleSection),
+   FOREIGN KEY(idProfesseur) REFERENCES Professeur(idProfesseur)
+);
+
+CREATE TABLE Eleve(
+   idEleve INT,
+   nomEleve VARCHAR(50) NOT NULL,
+   prenomEleve VARCHAR(50) NOT NULL,
+   mailEleve VARCHAR(50) NOT NULL,
+   adresseEleve VARCHAR(50) NOT NULL,
+   login VARCHAR(25),
+   idSection INT NOT NULL,
+   PRIMARY KEY(idEleve),
+   UNIQUE(mailEleve),
+   UNIQUE(login),
+   FOREIGN KEY(idSection) REFERENCES Section(idSection)
+);
+
+CREATE TABLE Notes(
+   idNotes INT,
+   note DECIMAL(3,2) NOT NULL CHECK (note <= 100.00 AND note >= 0.00),
+   idProfesseur INT NOT NULL,
+   idMatiere INT NOT NULL,
+   idEleve INT NOT NULL,
+   PRIMARY KEY(idNotes),
+   FOREIGN KEY(idProfesseur) REFERENCES Professeur(idProfesseur),
+   FOREIGN KEY(idMatiere) REFERENCES Matiere(idMatiere),
+   FOREIGN KEY(idEleve) REFERENCES Eleve(idEleve)
+);
+
+CREATE TABLE Prof_Matiere(
+   idProfesseur INT,
+   idMatiere INT,
+   PRIMARY KEY(idProfesseur, idMatiere),
+   FOREIGN KEY(idProfesseur) REFERENCES Professeur(idProfesseur),
+   FOREIGN KEY(idMatiere) REFERENCES Matiere(idMatiere)
+);
