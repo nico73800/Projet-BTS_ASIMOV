d6aaecfbb9c68227ee4111d9c7b9205e9fa20357
nico73800 <nico.chalo@outlook.fr>
2023-03-21 14:36:56 +0100

    màj connexion

--- sources/src/connexion_bdd.ts
+++ sources/src/connexion_bdd.ts
@@ -4,7 +4,7 @@ import * as iniparser from "iniparser";
 export let module_connexion =
         mysql.createConnection({
         host: 'localhost',
-        port:3308,
+        port:3306,
         user:'root',
         password:'root',
         database:'asimov'
