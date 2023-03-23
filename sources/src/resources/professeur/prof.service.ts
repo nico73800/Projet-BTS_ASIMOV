824f5d9db335ce609fad2c3c77ad542a644ac575
nico73800 <nico.chalo@outlook.fr>
2023-03-23 10:59:11 +0100

    ajouts notifications de connexions invaldides + menu

--- sources/src/resources/professeur/prof.service.ts
+++ sources/src/resources/professeur/prof.service.ts
@@ -9,13 +9,16 @@ let session;
 
 export function findProf(id: number, pwd: string, req: any, res: any) {
     if ((id == 0 || pwd == '') || id == undefined || pwd == undefined) {
-        res.render('connexion', {message: "Identifiant / mot de passe vide invalide "});
+        session = req.session;
+        session.message = "Identifiant / mot de passe vide invalide ";
+        res.render('connexion', {});
     } else {
         bdd.module_connexion.query("SELECT nomProfesseur, prenomProfesseur FROM professeur WHERE idProfesseur = ? AND password = SHA1(?)", [id, pwd] ,(err, result, fields) => {
             try {
                 if (result.toString() == '') {
                     session = req.session;
-                    session.uncorrectAuth = "Authentification incorrecte";
+                    session.message = "Authentification incorrecte";
+                    console.log(session);
                     res.redirect('/');
                 } else {
                     session=req.session;
