77023a638bcfc6ed169e692e5726cc3c8ef655ea
nico73800 <nico.chalo@outlook.fr>
2023-03-30 09:54:40 +0200

    ajout d'insert supplémentaire pour alimenter la base

--- Docs/table_asimov.sql
+++ Docs/table_asimov.sql
@@ -72,5 +72,30 @@ CREATE TABLE Prof_Matiere(
 
 
 -- Partie 2 / Insertion 
-INSERT INTO professeur (idProfesseur, nomProfesseur, prenomProfesseur, mailProfesseur, estReferent, estProviseur, password) VALUES ('', 'Louka', 'Lucas', 'louka.lucas@academ-gr.fr', '1', '1', SHA1('abcd'));
-INSERT INTO professeur (idProfesseur, nomProfesseur, prenomProfesseur, mailProfesseur, estReferent, estProviseur, password) VALUES ('', 'lulu', 'caca', 'lulu.caca@academ-gr.fr', '0', '0', SHA1('abcd'));
+INSERT INTO professeur (nomProfesseur, prenomProfesseur, mailProfesseur, estReferent, estProviseur, password) VALUES ('Louka', 'Lucas', 'louka.lucas@academ-gr.fr', '1', '1', SHA1('abcd'));
+
+INSERT INTO professeur (nomProfesseur, prenomProfesseur, mailProfesseur, estReferent, estProviseur, password) VALUES ('lulu', 'caca', 'lulu.caca@academ-gr.fr', '0', '0', SHA1('abcd'));
+
+INSERT INTO Eleve (idEleve, nomEleve, prenomEleve, mailEleve, adresseEleve, login, password, idSection) VALUES (NULL, 'bobi', 'boba', 'bobi.boba@mail.com', '1 rue des licornes, 12345 LicorneVille', 'bobib', 'abcd', '1');
+
+INSERT INTO Eleve (idEleve, nomEleve, prenomEleve, mailEleve, adresseEleve, login, password, idSection) VALUES (NULL, 'allé', 'mot', 'allé.motboba@mail.com', '1 rue des mot, 12345 motVille', 'allém', 'abcd', '1');
+
+INSERT INTO Matiere (idMatiere, libelle) VALUES (NULL, "Français");
+
+INSERT INTO Matiere (idMatiere, libelle) VALUES (NULL, "Anglais");
+
+INSERT INTO Matiere (idMatiere, libelle) VALUES (NULL, "Allemand");
+
+INSERT INTO Matiere (idMatiere, libelle) VALUES (NULL, "Mathématiques");
+
+INSERT INTO Section (libelleSection anneeSection, idProfesseur) VALUES("1ere STMG", "2020", 1);
+
+INSERT INTO Section (libelleSection, anneeSection, idProfesseur) VALUES("Term STMG", "2020", 1);
+
+INSERT INTO Section (libelleSection, anneeSection, idProfesseur) VALUES("1ere Spé LLCER", "2023", 1);
+
+INSERT INTO Section (libelleSection, anneeSection, idProfesseur) VALUES("2nd G", "2023", 1);
+
+INSERT INTO Prof_Matiere VALUES(1, 3);
+
+INSERT INTO Prof_Matiere (idProfesseur, idMatiere) VALUES ('2', '1');
