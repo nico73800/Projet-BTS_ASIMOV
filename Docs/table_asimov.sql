4bc81c39d2922c53ae1b745170477d60a71c8358
nico73800 <nico.chalo@outlook.fr>
2023-04-08 14:54:16 +0200

    update data from sql

--- Docs/table_asimov.sql
+++ Docs/table_asimov.sql
@@ -74,7 +74,7 @@ CREATE TABLE Prof_Matiere(
 -- Partie 2 / Insertion 
 INSERT INTO Professeur (nomProfesseur, prenomProfesseur, mailProfesseur, estReferent, estProviseur, password) VALUES ('Louka', 'Lucas', 'louka.lucas@academ-gr.fr', '1', '1', SHA1('abcd'));
 
-INSERT INTO Professeur (nomProfesseur, prenomProfesseur, mailProfesseur, estReferent, estProviseur, password) VALUES ('lulu', 'caca', 'lulu.caca@academ-gr.fr', '0', '0', SHA1('abcd'));
+INSERT INTO Professeur (nomProfesseur, prenomProfesseur, mailProfesseur, estReferent, estProviseur, password) VALUES ('Luca', 'Oh', 'luca.oh@academ-gr.fr', '0', '0', SHA1('abcd'));
 
 INSERT INTO Section (libelleSection, anneeSection, idProfesseur) VALUES("1ere STMG", "2020", 1);
 
