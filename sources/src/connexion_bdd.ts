e3d5f3227402f736de91feed2d919edfbbd281c6
nico73800 <nico.chalo@outlook.fr>
2023-04-09 09:44:26 +0200

    maj de l'indentation

--- sources/src/connexion_bdd.ts
+++ sources/src/connexion_bdd.ts
@@ -3,13 +3,10 @@ import * as iniparser from "iniparser";
 
 
 export let module_connexion =
-        mysql.createConnection({
-        host: 'localhost',
-        port:3306,
-        user:'root',
-        password:'root',
-        database:'Asimov'
-})
-
-// export const module_connexion = 
-//     mysql.createPool(options);
+	mysql.createConnection({
+		host: 'localhost',
+		port:3306,
+		user:'root',
+		password:'root',
+		database:'Asimov'
+	})
