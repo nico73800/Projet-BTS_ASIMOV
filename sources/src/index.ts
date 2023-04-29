97da564869ab619357393e9dfcadb64acc628d90
nico73800 <nico.chalo@outlook.fr>
2023-04-29 11:28:35 +0200

    ajout de commentaires

--- sources/src/index.ts
+++ sources/src/index.ts
@@ -20,16 +20,18 @@ import session from 'express-session';
 let app = express();
 let urlencodedParser = bodyParser.urlencoded({ extended: true })
 
-// Constante pour générer des UUID (identifiant unique universel)
+// Import pour générer des UUID (identifiant unique universel)
 import { v4 as uuid } from 'uuid';
 
+// Constante pour les Cross-origin resource sharing  
+// Probablement penser à supprimer ça si inutile 
 const corsOptions ={
     origin:'http://localhost:3000', 
     credentials:true,            
     optionSuccessStatus:200,
  }
  
- app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 // Paramétrage de l'app
 app.set('view engine', 'ejs');
@@ -39,6 +41,13 @@ app.use(express.static('src'));
 app.use(bodyParser.json());
 app.use(express.json());
 
+// Fonction de création d'un ID pour la session 
+let sess_id = function () {
+    console.log("Session ID Généré : " + uuid());
+    return uuid();
+}
+
+// Option mysql pour permettre la connexion à un serveur de stockage des sessions 
 const options = {
     connectionLimit: 10,
     user: 'root',
@@ -47,34 +56,41 @@ const options = {
     host: 'localhost',
 }
 
-// Fonction de création d'un ID pour la session 
-let sess_id = function () {
-    console.log("Session ID Généré : " + uuid());
-    return uuid();
-}
+// Inutilisé à l'heure actuelle 
+// const pool = mysql.createConnection(options);
 
-const pool = mysql.createConnection(options);
-
-// Création de la table session dans une base de données MySQL
-// Utile pour le stockage d'une session dans un serveur
+// constante permettant le stockage des sessions dans une base de données MySQL
 const MySQLStores = MySQLStore(expressSession);
+
+// constante instanciant la constante de stockage des sessions dans mysql (constante précédente) 
 const sessionsStore = new MySQLStores(options);
 
 
-// Paramétrage des sessions 
+// Paramétrage des sessions
 app.use(session({
+    // nom de la session
     name: "AsiNote",
+    // paramètre pour signer la session
     secret: "azertyuiop",
+    // paramètre pour stocker la session 
     store: sessionsStore,
+    // Force le "renouvellement" de la session à chaque changement 
     rolling: true,
+    // permet d'enregistrer des sessions vides
     saveUninitialized: true,
+    // génération des id de session
     genid: sess_id,
+    // Les cookies :
     cookie: {
+        // sécurisation de la page
         secure: false,
+        // durée d'expiration du cookie 
         expires: new Date(Date.now() + 86400000),
+        // Durée maximale d'existante du cookie 
         maxAge: 86400000,
         httpOnly: false
     },
+    // Force la session a être restocké même si elle n'a pas été modifiée 
     resave: true,
 }));
 
@@ -105,7 +121,7 @@ app.get('/', (req:Request, res:Response, next) => {
     res.render('choix_auth');
 });
 
-// Routage vers l'authentification pour le rôle de l'utilisateur demandé (élève ou prof)
+// Routage vers l'authentification pour le rôle de l'utilisateur demandé (élève ou prof ou le choix de l'utilisateur)
 app.post('/auth', urlencodedParser, (req:Request, res:Response, next) => {
     if (req.body.type == "prof") {
         req.session.typeSession = "prof";
@@ -119,18 +135,25 @@ app.post('/auth', urlencodedParser, (req:Request, res:Response, next) => {
     }
 });
 
-// Routage pour la gestion des erreurs (page inexistante, non autorisé, etc.)
+// Routage du bouton présent dans les views des erreurs (page inexistante, non autorisé, etc.)
 app.get('/redirect_handler', (req:Request, res:Response, NextFunction) => {
     console.log(req.session);
+    // Si quand le bouton est cliqué : 
+    // La section utilisateur existe + le type de session (aka le rôle de la personne (prof / élève)) n'est pas "undefined" 
+    // On redirige vers :
     if (typeof req.session.userid !== 'undefined' && typeof req.session.typeSession !== 'undefined') {
         console.log(req.session);
+        // La page prof
         if (req.session.typeSession === "prof") {
             res.redirect("/prof/accueil");
+        // La page élève (non implémentée)
         } else if (req.session.typeSession == "eleve") {
             res.redirect('/logout');
+        // On supprime les session + redirige vers l'accueil
         } else {
             res.redirect('/logout');
         }
+    // Si les deux param sont undefined on vide les sessions + on redirige vers l'accueil
     } else {
         res.redirect('/logout');
     }
